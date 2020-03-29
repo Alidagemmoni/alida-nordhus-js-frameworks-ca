43adeef001f5768f3d68cb8e1430390400228a99
Alidagemmoni <45143677+Alidagemmoni@users.noreply.github.com>
2020-03-29 23:06:33 +0200

    update search

--- src/components/home/SearchBox.js
+++ src/components/home/SearchBox.js
@@ -12,22 +12,6 @@ function Search({ doSearch }) {
     );
 }
 
-//import React from "react";
-//import { MDBCol } from "mdbreact";
-
-//const SearchPage = () => {
-  //return (
-    //<MDBCol md="6">
-      //<div className="active-pink-3 active-pink-4 mb-4">
-        //<input className="form-control" type="text" placeholder="Search" aria-label="Search" />
-     // </div>
-   // </MDBCol>
-  //);
-//}
-
-//export default SearchPage;
-
-
 Search.propTypes = {
     doSearch: PropTypes.func.isRequired
 };
